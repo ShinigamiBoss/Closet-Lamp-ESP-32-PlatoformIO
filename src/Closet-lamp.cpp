0155daddb473c9d62f481e22dd9ded3cc94b00e1
ShinigamiBoss <62751490+ShinigamiBoss@users.noreply.github.com>
2022-03-06 14:25:30 +0100

    Final application

--- src/Closet-lamp.cpp
+++ src/Closet-lamp.cpp
@@ -1,29 +1,37 @@
 #include <Arduino.h>
 #include "include/LED Parametes.h"
 
-//Button state read
+// Button state read
 uint16_t buttonState;
+int ButtonIn = 32;
 
 void setup()
 {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
-  //Set pins number
-  SetPins(23,22,21,19);
+  // Set pins number
+  SetPins(23, 22, 21, 19);
   InitializeLED();
+
+  //Set button input
+  pinMode(ButtonIn, INPUT_PULLUP);
+
+  
 }
 
 void loop()
 {
   // read the state of the pushbutton value
-  buttonState = analogRead(A6);
-
-    if (buttonState <= 200)
-    {
-      WriteLED(55, 221,215,0);
-    }
-    else
-    {
-      WriteLED(0, 0, 0,0);
-    }
+  buttonState = digitalRead(ButtonIn);
+  
+  Serial.println(buttonState);  
+  
+  if (buttonState == 1)
+  {
+    WriteLED(55, 221,215,0);
+  }
+  else
+  {
+    WriteLED(0, 0, 0,0);
+  }
 }
